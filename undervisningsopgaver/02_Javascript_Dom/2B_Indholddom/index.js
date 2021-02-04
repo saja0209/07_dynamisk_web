5c6f3c0616fde9d72b1a4f0db1aa52e635be6145
saja0209 <70881481+saja0209@users.noreply.github.com>
2021-02-04 18:28:14 +0100

    indhold skiftet af dom

---
+++ undervisningsopgaver/02_Javascript_Dom/2B_Indholddom/index.js
@@ -0,0 +1,26 @@
+const overskrift=document.querySelector("h1");
+overskrift.innerHTML="h1>LET US CHANGE THIS";
+
+const underoverskrift=document.querySelector("h2");
+underoverskrift.textContent="The section changed"
+
+const wrapper=document.querySelector(".wrapper");
+
+//Artikel 1
+
+const artikel1wrapper=document.querySelector(".article1");
+
+const img1=document.querySelector(".image1");
+
+const artikel1txt=document.querySelector(".txt1");
+
+//Artikel 2
+
+const artikel2wrapper=document.querySelector(".article2");
+
+const img2=document.querySelector(".image2");
+
+const artikel2txt=document.querySelector(".txt2")
+
+//footer
+const fod=document.querySelector("footer");
