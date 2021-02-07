dbb3c48d9bbf566843662f291424bbecde49b484
saja0209 <70881481+saja0209@users.noreply.github.com>
2021-02-07 23:53:37 +0100

    alle dyr i array

---
+++ undervisningsopgaver/04_Javascript_Objekter/04_DyrArray/kode.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', ()=>console.log("hello"));
+
+
+let dyr ={"navn":"Sebbe","type":"Husky","Billede":"https://www.rd.com/wp-content/uploads/2021/01/GettyImages-1257560163-scaled-e1610062322469.jpg","levested":"Alaska"};
+
+
+
+let fleredyr=[{"navn":"gris","type":"pattedyr","billede":"http://mabe-kea.dk/E19/pics/pig.png"},{"navn":"frø","type":"amfibie","billede":"http://mabe-kea.dk/E19/pics/frog.png"},{"navn":"ræv","type":"pattedyr","billede":"http://mabe-kea.dk/E19/pics/fox.png"},{"navn":"sild","type":"fisk","billede":"http://mabe-kea.dk/E19/pics/sild.jpg"},{"navn":"krokodille","type":"krybdyr","billede":"http://mabe-kea.dk/E19/pics/kroko.jpg"}];
+console.log(fleredyr);
