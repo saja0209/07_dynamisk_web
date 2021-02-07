e97468597f4d4473d33dd70c3352081b45301341
saja0209 <70881481+saja0209@users.noreply.github.com>
2021-02-07 22:59:14 +0100

    Dyr1

---
+++ undervisningsopgaver/04_Javascript_Objekter/01_Dyreobjekt/kode.js
@@ -0,0 +1,5 @@
+let dyr ={"navn":"Sebbe","type":"Husky","Billede":"https://www.rd.com/wp-content/uploads/2021/01/GettyImages-1257560163-scaled-e1610062322469.jpg","levested":"Alaska"};
+
+const placering =document.querySelector("article");
+
+console.log(`Fornavn: ${dyr.navn},Type:${dyr.type},Levested:${dyr.levested}, Billede:${dyr.Billede}`);
