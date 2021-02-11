3b658c83304393d83cceb9dc4920fe8f8cee1c7a
saja0209 <70881481+saja0209@users.noreply.github.com>
2021-02-11 22:41:07 +0100

    bistro

--- Bistro/script.js
+++ Bistro/script.js
@@ -1,12 +1,12 @@
   const header = document.querySelector("header h1");
   const url = "https://babushka-dd8a.restdb.io/rest/menu"
-  const medieurl = "https://babushka-dd8a.restdb.io/media";
+  const medieurl = "https://babushka-dd8a.restdb.io/media/";
   const myHeaders = {
 
       "x-apikey": "600ec2fb1346a1524ff12de4"
   }
   document.addEventListener("DOMContentLoaded", start)
-  let personer;
+  let retter;
 
 
   // første funktion der kaldes efter DOM er loaded
@@ -16,16 +16,38 @@
   }
 
   async function loadJSON() {
+      console.log("loadJSON");
       const JSONData = await fetch(url, {
           headers: myHeaders
       });
-      personer = await JSONData.json();
-      console.log("Personer", personer);
-      visPersoner();
+      retter = await JSONData.json();
+      console.log("retter", retter);
+      visRetter();
   }
 
   //funktion der viser personer i liste view
-  function visPersoner() {
-console.log(personer);
+  function visRetter() {
+      console.log("visRetter");
+      const dest = document.querySelector("#liste");
+      const skabelon = document.querySelector("template").content;
+      console.log(skabelon);
 
+
+      retter.forEach(ret => {
+          const klon = skabelon.cloneNode(true);
+          console.log(klon);
+          klon.querySelector("h2").textContent = ret.navn;
+          klon.querySelector("img").src = medieurl + ret.billede;
+          klon.querySelector(".beskrivelse").textContent = "Beskrivelse: " + ret.kortbeskrivelse;
+          klon.querySelector(".pris").textContent = "Pris " + ret.pris + ".-";
+          klon.querySelector(".ret").addEventListener("click", () => {
+              menuClick(ret);
+          })
+          dest.appendChild(klon);
+      })
+  }
+
+  function menuClick(ret) {
+      console.log("hello", ret._id);
+      location.href = "02_detalje.html?id=" + ret._id;
   }
